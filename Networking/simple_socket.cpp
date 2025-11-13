50f7e8e20c422469c14e091f31e3e18ac35cdf00
Pradip Gyawali <gyawalipradip846@gmail.com>
2025-11-13 22:13:56 +0545

    Refactor SimpleSocket class to improve connection handling and add missing includes

--- Networking/simple_socket.cpp
+++ Networking/simple_socket.cpp
@@ -6,5 +6,23 @@ HDE::SimpleSocket::SimpleSocket(int domain,int service,int protocal,u_long inter
     address.sin_port=htons(port);
     address.sin_addr.s_addr=htonl(interface)
     sock=socket(domain,service,protocal);
-    connect=connect_to_network();
+    test_connection(sock);
+    connect=connect_to_network(sock,address);
+    test_connection(connect);
+}-
+void HDE::SimpleSocket::test_connection(int item_to_test){
+    if(item_to_test<0){
+        perror("Failed for sockect estblishment ...");
+        exit(EXIT_FAILURE);
+    }
+}
+
+struct sockaddr_in HDE::SimpleSocket::get_address(){
+    return address;
+}
+int sockaddr_in HDE::SimpleSocket::get_sock(){
+    return sock;
+}
+int sockaddr_in HDE::SimpleSocket::get_connection(){
+    return connection;
 }
--- Networking/simple_socket.hpp
+++ Networking/simple_socket.hpp
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include<sys/socket.h>
 #include<netinet/in.h>
+#include<iostream>
 
 namespcae HDE{
     class SimpleSocket{
@@ -12,7 +13,11 @@ namespcae HDE{
         struct sockaddr_in address;
         public:
         SimpleSocket(int domain,int service,int protocal,u_long interface);//The constructor is used to take the argument for the socket creation
-        virtual int connect_to_network()=0;
+        virtual int connect_to_network(int sock ,struct sockaddr_in address)=0;//to make scure the bind and connect is called dependng the server or client 
+        void test_connection(int);
+        struct sockaddr_in get_address();
+        int get_sock();
+        int get_connection();
     }
 }
 #endif
