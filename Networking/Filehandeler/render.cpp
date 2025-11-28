c2c92fc551837b47fd8dfe8a232e849f34288ed6
Pradip Gyawali <gyawalipradip846@gmail.com>
2025-11-29 00:28:16 +0545

    Add hello.html template and update webserver binary
    - Introduced a new HTML template (hello.html) in both build/templates and templates directories.
    - Updated the webserver binary to reflect recent changes.

--- Networking/Filehandeler/render.cpp
+++ Networking/Filehandeler/render.cpp
@@ -6,12 +6,11 @@
 #include "../Server/TestServer.hpp"
 
 std::string render(const HDE::RequestHandler &request, const std::string &path) {
-    std::string full_path = path + ".html";
-    std::cout << "Rendering file: " << full_path << std::endl;
-    std::ifstream in(full_path,std::ios::in);
+    std::string full_path = "templates/" + path + ".html";  
+    std::ifstream in(full_path, std::ios::in);
     if (!in) {
         std::cout << "No such file found: " << full_path << std::endl;
-        return "<h1>404 File Not Found </h1>";
+        return "<h1>404 File Not Found</h1>";
     }
 
     std::stringstream buffer;
